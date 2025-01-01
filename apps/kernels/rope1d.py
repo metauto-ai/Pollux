f8f72a257d8768e921ecbb262f89f68565386530
shaRk-033 <96977927+shaRk-033@users.noreply.github.com>
2025-01-01 22:05:07 +0530

    smoll cleanup

--- apps/kernels/rope1d.py
+++ apps/kernels/rope1d.py
@@ -1,23 +1,6 @@
 import triton
 import torch
 import triton.language as tl
-from typing import Tuple
-
-MAX_FUSED_SIZE: int = 65536
-
-def calculate_settings(n: int) -> Tuple[int, int]:
-    BLOCK_SIZE: int = triton.next_power_of_2(n)
-    if BLOCK_SIZE > MAX_FUSED_SIZE:
-        raise RuntimeError(f"Cannot launch Triton kernel since n = {n} exceeds "
-                         f"the maximum CUDA blocksize = {MAX_FUSED_SIZE}.")
-    num_warps: int = 4
-    if BLOCK_SIZE >= 32768:
-        num_warps = 32
-    elif BLOCK_SIZE >= 8192:
-        num_warps = 16
-    elif BLOCK_SIZE >= 2048:
-        num_warps = 8
-    return BLOCK_SIZE, num_warps
 
 def rope1d(x, head_dim):
     theta = 10000 ** (-2 * torch.arange(0, head_dim, 2, dtype=torch.float32) / head_dim)
