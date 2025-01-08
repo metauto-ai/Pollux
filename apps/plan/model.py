2bb8eb210602ac0c2a502dd5cd5ada4f6f3b0404
mczhuge <mczhuge@gmail.com>
2025-01-08 19:23:00 +0000

    update the forward

--- apps/plan/model.py
+++ apps/plan/model.py
@@ -46,13 +46,14 @@ logger = logging.getLogger()
 @dataclass
 class LLMArgs:
     model_name: str
+    hidden_size: int
 
 
 @dataclass
 class LatentProjecterArgs:
     latent_dim: int = 16
     output_dim: int = 3072
-    patch_size: int = 2
+    patchify_size: int = 1
 
 
 @dataclass
@@ -66,6 +67,7 @@ class ModelArgs:
     text_cfg_ratio: float = 0.1
     image_cfg_ratio: float = 0.1
     scheduler: SchedulerArgs = field(default_factory=SchedulerArgs)
+    num_classes: int = 1000
 
 
 class Pollux(nn.Module):
@@ -79,37 +81,37 @@ class Pollux(nn.Module):
         super().__init__()
         self.args = args
 
-        # vision encoder
         self.vae = LatentVideoVAE(args.vae)
-
-        # latent projector
-        self.patch_size = args.latent_projector.patch_size
+        self.patchify_size = args.latent_projector.patchify_size
         self.latent_projector = nn.Linear(
-            self.patch_size**2 * args.latent_projector.latent_dim,
+            self.patchify_size**2 * args.latent_projector.latent_dim,
             args.latent_projector.output_dim,
             bias=False,
         )
 
-        # vision boi embedding
-        if args.use_vision_boi:
-            self.vision_boi_emb = nn.Parameter(
-                torch.zeros(1, args.latent_projector.output_dim)
-            )
+        self.vision_cls_emb = LabelEmbedder(
+            num_classes=args.num_classes,
+            hidden_size=args.llm.hidden_size,
+            dropout_prob=args.image_cfg_ratio,
+        )
 
-        # llm
+        self.vision_boi_emb = nn.Parameter(torch.zeros(1, args.latent_projector.output_dim))
+        self.vision_eoi_emb = nn.Parameter(torch.zeros(1, args.latent_projector.output_dim))
+
         self.llm_tokenizer = LlamaTokenizerFast.from_pretrained(args.llm.model_name)
         self.llm = LlamaForCausalLM.from_pretrained(args.llm.model_name)
         if self.llm_tokenizer.pad_token is None:
             self.llm_tokenizer.pad_token = self.llm_tokenizer.eos_token
 
-        # head
         self.latent_head = nn.Linear(
             args.latent_projector.output_dim,
-            self.patch_size**2 * args.latent_projector.latent_dim,
+            self.patchify_size**2 * args.latent_projector.latent_dim,
         )
 
+
+    
     def patchify_and_embed(self, x: torch.Tensor):
-        pH = pW = self.patch_size
+        pH = pW = self.patchify_size
         B, C, H, W = x.size()
         x = x.view(B, C, H // pH, pH, W // pW, pW).permute(0, 2, 4, 1, 3, 5).flatten(3)
         x = self.latent_projector(x)
@@ -118,7 +120,8 @@ class Pollux(nn.Module):
 
     def unpatchify_image(self, x: torch.Tensor, H: int, W: int) -> torch.Tensor:
         B = x.size(0)
-        pH = pW = self.patch_size
+        pH = pW = self.patchify_size
+
         x = x.view(B, H // pH, W // pW, pH, pW, -1)
         x = x.permute(0, 5, 1, 3, 2, 4).flatten(4, 5).flatten(2, 3)  # [B,16,H/8,W/8]
         return x
@@ -170,32 +173,31 @@ class Pollux(nn.Module):
         random_rate: float = 0.3,
     ) -> Tuple[dict[str, any], torch.Tensor]:
 
-        images = batch["image"]  # Shape: [B, C, H, W]
+        images = batch["image"]
+        labels = batch["label"]
         captions = batch["caption"]
 
-        # vision vae
-        vae_indices, vae_latent = self.vae.encode(images)  # [B,16,H/8,W/8]
-        vae_embed, H_, W_ = self.patchify_and_embed(vae_latent)  # [B,H/16*W/16,D]
+        # Class Embedding
+        cls_emb = self.vision_cls_emb(labels, train=True).unsqueeze(1)  # [B, 1, D]
 
-        # Input Shape: torch.Size([64, 3, 256, 256])
-        # Indices Shape: torch.Size([64, 1, 16, 16])
-        # Codes Shape: torch.Size([64, 6, 1, 16, 16])
-        # Reconstructed Shape: torch.Size([64, 3, 256, 256])
+        # Vision Discrete Latent Codes
+        vae_indices, vae_latent = self.vae.encode(images)  # [B, 1, H/16, W/16], [B, 6, 1, H/16, W/16]
+        vae_embs, H_, W_ = self.patchify_and_embed(vae_latent.squeeze(2)) # [B, H/16*W/16, D]
 
-        # text embed
+        # Text Embedding
         input_ids = self.llm_tokenizer(
             captions, padding=True, truncation=True, return_tensors="pt"
         )["input_ids"].to(self.llm.device)
         text_embs = self.llm.get_input_embeddings()(input_ids)  # [B,T,D]
 
-        # concate embeddings
-        boi_embs = self.vision_boi_emb.unsqueeze(0).expand(vae_embed.size(0), -1, -1)
-        mm_embs = torch.cat(
-            [text_embs, boi_embs, vae_embed], dim=1
-        )  # multi-modal embeddings
-        vae_start_idx = text_embs.size(1) + 1
+        # Concat
+        boi_emb = self.vision_boi_emb.unsqueeze(0).expand(vae_embs.size(0), -1, -1)
+        eoi_emb = self.vision_eoi_emb.unsqueeze(0).expand(vae_embs.size(0), -1, -1)
 
-        # llm forward
+        mm_embs = torch.cat([text_embs, cls_emb, boi_emb, vae_embs, eoi_emb], dim=1)
+        vae_start_idx = text_embs.size(1) + 2
+
+        # LLM Forward
         output = self.llm(
             inputs_embeds=mm_embs,
             # attention_mask=attention_mask,
@@ -204,13 +206,13 @@ class Pollux(nn.Module):
             return_dict=True,
         )
 
-        # latent head
-        latent_hidden = output.hidden_states[-1][:, vae_start_idx:, :]  # [B,M,D]
+        # Latent Head
+        latent_hidden = output.hidden_states[-1][:, vae_start_idx:vae_start_idx+vae_embs.size(1), :]  # [B,M,D]
         pred_latent = self.latent_head(latent_hidden)
-        pred_latent = self.unpatchify_image(pred_latent, H_, W_)  # [B,16,H/8,W/8]
+        pred_latent = self.unpatchify_image(pred_latent, H_, W_) 
 
         # compute loss
-        pred_loss = F.mse_loss(pred_latent, vae_latent)
+        pred_loss = F.mse_loss(pred_latent, vae_latent.squeeze(2))
 
         # batch["latent_target"] = latent_target
         # batch["pred_latent"] = pred_latent
@@ -218,9 +220,13 @@ class Pollux(nn.Module):
 
     def set_train(self):
         self.latent_projector.train()
+        self.vision_cls_emb.train()
+        self.llm.train()
 
     def set_eval(self):
         self.latent_projector.eval()
+        self.vision_cls_emb.eval()
+        self.llm.eval()
 
     def init_weights(self, args: ModelArgs):
 
@@ -232,9 +238,11 @@ class Pollux(nn.Module):
 
         nn.init.xavier_uniform_(self.latent_projector.weight)
         nn.init.normal_(self.vision_boi_emb, mean=0.0, std=0.02)
+        nn.init.normal_(self.vision_eoi_emb, mean=0.0, std=0.02)
         nn.init.xavier_uniform_(self.latent_head.weight)
         nn.init.zeros_(self.latent_head.bias)
 
+        self.vision_cls_emb.reset_parameters()
 
 # Optional policy for activation checkpointing. With None, we stick to the default (defined distributed.py: default_no_recompute_ops)
 def get_no_recompute_ops():
