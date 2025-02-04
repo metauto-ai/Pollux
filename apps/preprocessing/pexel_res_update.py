380821b23ba9f31ebeca6a902c6854e50355d2ac
HaozheLiu-ST <liuhaozhest@gmail.com>
2025-02-04 13:48:03 +0300

    update res and rebuild bucket separately.

--- apps/preprocessing/pexel_res_update.py
+++ apps/preprocessing/pexel_res_update.py
@@ -5,7 +5,7 @@
 # mongoexport --uri="mongodb+srv://nucleusadmin:eMPF9pgRy2UqJW3@nucleus.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000" \
 # --db=world_model \
 # --collection=pexel_images \
-# --out=/mnt/pollux/mongo_db_cache/pexel_images.json --jsonArray
+# --out=/tmp/pexel_images.json --jsonArray
 # mongoimport --uri="mongodb+srv://nucleusadmin:eMPF9pgRy2UqJW3@nucleus.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000" \
 # --db=world_model \
 # --collection=bucket-hq \
@@ -15,12 +15,14 @@ import json
 import requests
 from PIL import Image
 import io
-from joblib import Parallel, delayed
+from joblib import Parallel, delayed, parallel_backend
 import random
 from tqdm import tqdm
 from tqdm_joblib import tqdm_joblib
+from pymongo import MongoClient
+from bson import ObjectId
 
-file_path = "/mnt/pollux/mongo_db_cache/pexel_images.json"
+file_path = "/tmp/pexel_images.json"
 
 
 def update_doc(doc):
@@ -51,14 +53,45 @@ def update_doc(doc):
         return None  # Return None or some default value in case of error
 
 
+URI = "mongodb+srv://nucleusadmin:eMPF9pgRy2UqJW3@nucleus.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+mongodb_client = MongoClient(URI)
+db = mongodb_client["world_model"]
+collection = db["pexel_images"]
+
+
+def submit_doc(doc):
+    update_data = {}
+    if "nova_lite_caption" not in doc:
+        return 0
+    if "partition_key" not in doc:
+        partition_key = random.randint(0, 10000)
+        update_data = {"partition_key": partition_key}
+    try:
+        response = requests.get(doc["url"])
+        image = Image.open(io.BytesIO(response.content)).convert("RGB")
+        width, height = image.size
+        update_data["width"] = width
+        update_data["height"] = height
+
+        update_query = {"$set": update_data}
+        filter_query = {"_id": ObjectId(doc["_id"]["$oid"])}
+        collection.update_one(filter_query, update_query)
+        return 1
+    except Exception as e:
+        print(f"Error processing element {doc['_id']}: {e}")
+        return 0  # Return None or some default value in case of error
+
+
 with open(file_path, "r", encoding="utf-8") as f:
     data = json.load(f)  # Load JSON array
-
     with tqdm_joblib(tqdm(desc="Processing", total=len(data))):
-        processed_results = Parallel(n_jobs=32)(delayed(update_doc)(el) for el in data)
-    processed_results = [res for res in processed_results if res is not None]
-    with open(
-        "/mnt/pollux/mongo_db_cache/pexel_images_processed.json", "w", encoding="utf-8"
-    ) as f:
-        json.dump(processed_results, f, indent=4)
+        with parallel_backend("threading"):
+            processed_results = Parallel(n_jobs=16)(
+                delayed(submit_doc)(el) for el in data
+            )
+    # processed_results = [res for res in processed_results if res is not None]
+    # with open(
+    #     "/mnt/pollux/mongo_db_cache/pexel_images_processed.json", "w", encoding="utf-8"
+    # ) as f:
+    #     json.dump(processed_results, f, indent=4)
 print("Processing finished")
