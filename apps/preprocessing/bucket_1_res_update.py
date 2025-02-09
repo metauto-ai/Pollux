e3710d59a2fc07d85db1fb49355193cf04929031
HaozheLiu-ST <liuhaozhest@gmail.com>
2025-02-10 00:27:38 +0300

    support memory efficient data management

--- apps/preprocessing/bucket_1_res_update.py
+++ apps/preprocessing/bucket_1_res_update.py
@@ -2,10 +2,10 @@
 # echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 # sudo apt update
 # sudo apt install mongodb-database-tools
-# mongoexport --uri="mongodb+srv://nucleusadmin:eMPF9pgRy2UqJW3@nucleus.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000" \
+# mongoexport --uri="mongodb+srv://nucleusadmin:eMPF9pgRy2UqJW3@imagedata.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000" \
 # --db=world_model \
-# --collection=pexel_images \
-# --out=/tmp/pexel_images.json --jsonArray
+# --collection=bucket-256-1 \
+# --out=/data/bucket-256-1.json --jsonArray
 # mongoimport --uri="mongodb+srv://nucleusadmin:eMPF9pgRy2UqJW3@nucleus.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000" \
 # --db=world_model \
 # --collection=bucket-hq \
@@ -21,8 +21,11 @@ from tqdm import tqdm
 from tqdm_joblib import tqdm_joblib
 from pymongo import MongoClient
 from bson import ObjectId
+import mmap
+import orjson
+import ijson
 
-file_path = "/tmp/bucket-256-1.json"
+file_path = "/data/bucket-256-1.json"
 
 
 URI = "mongodb+srv://nucleusadmin:eMPF9pgRy2UqJW3@imagedata.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
@@ -32,7 +35,7 @@ collection = db["bucket-256-1"]
 
 
 def submit_doc(doc):
-    if "width" or "height" in doc:
+    if "width" in doc and "height" in doc:
         return 0
     else:
         try:
@@ -52,16 +55,18 @@ def submit_doc(doc):
             return 0  # Return None or some default value in case of error
 
 
-with open(file_path, "r", encoding="utf-8") as f:
-    data = json.load(f)  # Load JSON array
-    with tqdm_joblib(tqdm(desc="Processing", total=len(data))):
-        with parallel_backend("threading"):
-            processed_results = Parallel(n_jobs=16)(
-                delayed(submit_doc)(el) for el in data
-            )
+with open(file_path, "rb") as f:
+    objects = ijson.items(f, "item")  # Stream each JSON object one-by-one
+
+    with parallel_backend("threading"):
+        Parallel(n_jobs=16)(
+            delayed(submit_doc)(obj) for obj in tqdm(objects, desc="Processing")
+        )
     # processed_results = [res for res in processed_results if res is not None]
     # with open(
     #     "/mnt/pollux/mongo_db_cache/pexel_images_processed.json", "w", encoding="utf-8"
     # ) as f:
     #     json.dump(processed_results, f, indent=4)
+
+
 print("Processing finished")
