163b82c274bc7b671dd4fc25544cb187279cd5be
HaozheLiu-ST <liuhaozhest@gmail.com>
2025-02-12 12:06:49 +0300

    add download_fineweb script

---
+++ apps/preprocessing/fine_web_downloading.py
@@ -0,0 +1,9 @@
+from huggingface_hub import snapshot_download
+
+folder = snapshot_download(
+    "HuggingFaceFW/fineweb",
+    repo_type="dataset",
+    local_dir="/mnt/pollux/language_data/",
+    # replace "data/CC-MAIN-2023-50/*" with "sample/100BT/*" to use the 100BT sample
+    allow_patterns="sample/100BT/*",
+)
