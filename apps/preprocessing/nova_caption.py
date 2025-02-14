bebfd0ed65eec7b9ff148646be4d00e3eac662dc
HaozheLiu-ST <liuhaozhest@gmail.com>
2025-02-14 23:16:58 +0300

    update nova_caption

--- apps/preprocessing/nova_caption.py
+++ apps/preprocessing/nova_caption.py
@@ -205,7 +205,7 @@ if __name__ == "__main__":
     batch_size = 32
     max_samples_per_min = 500
     nova_caption = NovaCaption(
-        collection_name="flickr-part-00-of-08",
+        collection_name="flickr-part-01-of-08",
         image_field="AZURE_URL",
         caption_field="nova_lite_caption",
         maxTokens=150,
