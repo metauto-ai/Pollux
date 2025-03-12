21e6c080cb943a5145af41fc9fb88a103c96a06c
Murali Nandan <nmn@withnucleus.ai>
2025-03-12 07:29:52 +0000

    dynamic cropping

--- apps/main/modules/preprocess.py
+++ apps/main/modules/preprocess.py
@@ -158,12 +158,55 @@ def center_crop_arr(pil_image, image_size):
         tuple(round(x * scale) for x in pil_image.size), resample=Image.BICUBIC
     )
 
+    crop_size = var_center_crop_size_fn(pil_image.size, image_size)
+
     arr = np.array(pil_image)
-    crop_y = (arr.shape[0] - image_size) // 2
-    crop_x = (arr.shape[1] - image_size) // 2
+    crop_y = (arr.shape[0] - crop_size[0]) // 2
+    crop_x = (arr.shape[1] - crop_size[1]) // 2
     return Image.fromarray(
-        arr[crop_y : crop_y + image_size, crop_x : crop_x + image_size]
+        arr[crop_y : crop_y + crop_size[0], crop_x : crop_x + crop_size[1]]
     )
+
+
+def generate_crop_size_list(image_size, patch_size, max_ratio=2.0):
+    assert max_ratio >= 1.0
+    min_wp, min_hp = image_size // patch_size, image_size // patch_size
+    crop_size_list = []
+    wp, hp = min_wp, min_hp
+    while hp / wp <= max_ratio:
+        crop_size_list.append((wp * patch_size, hp * patch_size))
+        hp += 1
+    wp, hp = min_wp + 1, min_hp
+    while wp / hp <= max_ratio:
+        crop_size_list.append((wp * patch_size, hp * patch_size))
+        wp += 1
+    return crop_size_list
+
+
+def is_valid_crop_size(cw, ch, orig_w, orig_h):
+    down_scale = max(cw / orig_w, ch / orig_h)
+    return cw <= orig_w * down_scale and ch <= orig_h * down_scale
+
+
+def var_center_crop_size_fn(orig_img_shape, image_size=256, patch_size=16, max_ratio=2.0):
+    """
+    Dynamic cropping from Lumina-Image-2.0
+    https://github.com/Alpha-VLLM/Lumina-Image-2.0/blob/main/imgproc.py#L39
+    """
+    w, h = orig_img_shape[:2]
+    crop_size_list = generate_crop_size_list(
+        image_size=image_size, 
+        patch_size=patch_size, 
+        max_ratio=max_ratio
+    )
+    rem_percent = [
+        min(cw / w, ch / h) / max(cw / w, ch / h) 
+        if is_valid_crop_size(cw, ch, w, h) else 0 
+        for cw, ch in crop_size_list
+    ]
+    crop_size = sorted(((x, y) for x, y in zip(rem_percent, crop_size_list) if x > 0), reverse=True)[0][1]
+    return np.array(crop_size, dtype=np.float32)
+
 
 
 ######################## FOR TEXT ########################
